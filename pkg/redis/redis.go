56db9df5ab60b76c5dea3334e3dc455e483a80a3
James Bond <introbond.dev@gmail.com>
2024-06-30 12:42:17 +0700

    WIP: Redis client basic cmd

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -2,20 +2,32 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
+const (
+	Timeout = 1
+)
+
 type (
 	Client interface {
+		IsConnected() bool
+
+		Get(key string, value interface{}) error
+		Set(key string, value interface{}) error
+		SetWithExpiration(key string, value interface{}, expiration time.Duration) error
+		Remove(keys ...string) error
+
 		Publish(channel string, message interface{}) error
 		Subscribe(channel string) (<-chan *redis.Message, error)
 	}
 
 	client struct {
 		client *redis.Client
-		ctx    context.Context
 	}
 
 	Message = redis.Message
@@ -29,14 +41,15 @@ type (
 )
 
 func NewClient(config Config) (Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     config.Address,
 		Username: config.Username,
 		Password: config.Password,
 		DB:       config.DB,
 	})
-
-	ctx := context.Background()
 
 	_, err := rdb.Ping(ctx).Result()
 	if err != nil {
@@ -47,20 +60,123 @@ func NewClient(config Config) (Client, error) {
 
 	return &client{
 		client: rdb,
-		ctx:    ctx,
 	}, nil
 }
 
-func (c *client) Publish(channel string, message interface{}) error {
-	return c.client.Publish(c.ctx, channel, message).Err()
+func (r *client) IsConnected() bool {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	if r.client == nil {
+		return false
+	}
+
+	pong, err := r.client.Ping(ctx).Result()
+	log.Fatal(pong, err)
+	return err == nil
 }
 
-func (c *client) Subscribe(channel string) (<-chan *redis.Message, error) {
-	pubsub := c.client.Subscribe(c.ctx, channel)
-	_, err := pubsub.Receive(c.ctx)
+func (r *client) Publish(channel string, message interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	return r.client.Publish(ctx, channel, message).Err()
+}
+
+func (r *client) Subscribe(channel string) (<-chan *redis.Message, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	pubsub := r.client.Subscribe(ctx, channel)
+	_, err := pubsub.Receive(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	return pubsub.Channel(), nil
+}
+
+func (r *client) Get(key string, value interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	strValue, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(strValue), value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *client) Set(key string, value interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	bData, _ := json.Marshal(value)
+	err := r.client.Set(ctx, key, bData, 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *client) SetWithExpiration(key string, value interface{}, expiration time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	bData, _ := json.Marshal(value)
+	err := r.client.Set(ctx, key, bData, expiration).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *client) Remove(keys ...string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	err := r.client.Del(ctx, keys...).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *client) Keys(pattern string) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout*time.Second)
+	defer cancel()
+
+	keys, err := r.client.Keys(ctx, pattern).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return keys, nil
+}
+
+func (r *client) RemovePattern(pattern string) error {
+	keys, err := r.Keys(pattern)
+	if err != nil {
+		return err
+	}
+
+	if len(keys) == 0 {
+		return nil
+	}
+
+	err = r.Remove(keys...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
