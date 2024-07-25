c4afa76848149a33fd7fcab637efdb8d32a096ce
James Bond <introbond.dev@gmail.com>
2024-07-25 18:10:44 +0700

    Refactor

--- pkg/sse/sse.go
+++ pkg/sse/sse.go
@@ -4,24 +4,47 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-// StreamSSE handles Server-Sent Events for the given context and events channel.
-func StreamSSE[T any](ctx context.Context, w http.ResponseWriter, events chan T) {
+// setSSEHeaders sets the necessary headers for Server-Sent Events.
+func setSSEHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Transfer-Encoding", "chunked")
+}
 
-	w.(http.Flusher).Flush()
+// logError logs errors with a consistent format.
+func logError(message string, err error) {
+	if err != nil {
+		log.Printf("ERROR: %s: %v\n", message, err)
+	}
+}
+
+// StreamSSE handles Server-Sent Events for the given context and events channel.
+func StreamSSE[T any](ctx context.Context, w http.ResponseWriter, events chan T) {
+	setSSEHeaders(w)
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		log.Fatal("Response writer does not support flushing")
+	}
+
+	flusher.Flush()
 
 	for {
 		select {
-		case event := <-events:
+		case event, ok := <-events:
+			if !ok {
+				log.Println("Event channel closed")
+				return
+			}
+
 			eventData, err := json.Marshal(event)
 			if err != nil {
-				fmt.Printf("Error encoding event data: %v\n", err)
+				logError("Error encoding event data", err)
 				continue
 			}
 
@@ -29,14 +52,14 @@ func StreamSSE[T any](ctx context.Context, w http.ResponseWriter, events chan T)
 
 			_, err = fmt.Fprint(w, eventStr)
 			if err != nil {
-				fmt.Printf("Error writing to client: %v. Closing HTTP connection.\n", err)
+				logError("Error writing to client", err)
 				return
 			}
 
-			w.(http.Flusher).Flush()
+			flusher.Flush()
 
 		case <-ctx.Done():
-			fmt.Println("Client connection closed.")
+			log.Println("Client connection closed.")
 			return
 		}
 	}
